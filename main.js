cb5b869c918a20eb07e8064f52417d92e4094e66
Quentin Thomas <qmanthomas@gmail.com>
2015-02-02 15:45:46 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+(function() {
+    var answer = prompt("Which Number should I end Fizz Buzz with.");
+    number = parseInt(answer);
+    if (number >= 1) {
+        for (var i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i & 5 == 0) {
+                document.write("FizzBuzz");
+            } else if (i % 3 == 0) {
+                document.write("Fizz");
+            } else if (i % 5 == 0) {
+                document.write("Buzz");
+            } else {
+                document.write(i);
+            }
+        }
+    } else {
+        alert("You must enter a number greater than 1 with no decimal points to begin the fizz buzz challenge.")
+    }
+})();
